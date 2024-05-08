5a0970306d569ccf026799d0d184add7d589a7aa
vaeIyx <digonzal@andrew.cmu.edu>
2024-05-08 17:45:02 -0400

    Adding files x2

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "https://dash.cloudflare.com/96dfbcd8a0eb0e1dbfb209be6877013f/workers/services/view/simple-proxy/production",
+  VITE_CORS_PROXY_URL: "https://simple-proxy.dig135603451.workers.dev/",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNmQwODRkM2FlNTRkYmFmMjFjM2VmMmVhZTI2OWRlZCIsInN1YiI6IjY2M2JlZTZiYWM3M2FkOGU2MDg4ODU5NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JmGI1Do9Nqumk6qkpuRhHT4vja6smA5fmAocXNZzzeE",
